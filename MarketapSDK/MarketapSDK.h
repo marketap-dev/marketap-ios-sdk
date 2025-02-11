8916719defe1a47738db0fd7b2d581a8b651613a
donghyun-marketap <donghyun.lee@marketap.io>
2025-02-11 17:18:23 +0900

    Initial Commit

---
+++ MarketapSDK/MarketapSDK.h
@@ -0,0 +1,18 @@
+//
+//  MarketapSDK.h
+//  MarketapSDK
+//
+//  Created by 이동현 on 2/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarketapSDK.
+FOUNDATION_EXPORT double MarketapSDKVersionNumber;
+
+//! Project version string for MarketapSDK.
+FOUNDATION_EXPORT const unsigned char MarketapSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarketapSDK/PublicHeader.h>
+
+
