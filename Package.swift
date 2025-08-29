a2b1d6bd203d108d8324900dc56876a0b8d97ddf
donghyun-marketap <donghyun.lee@marketap.io>
2025-08-29 21:45:13 +0900

    temp commit

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "MarketapSDK",
+            type: .dynamic,
             targets: ["MarketapSDK"]
         ),
         .library(
